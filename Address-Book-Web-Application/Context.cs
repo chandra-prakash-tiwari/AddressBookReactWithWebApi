1b422747430bc68c6f5c0166389baa76b9dc2a14
Chandra Prakash Tiwari <tiwarivas88@gmail.com>
2020-04-19 16:00:52 +0530

    commit

---
+++ Address-Book-Web-Application/Context.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Address_Book_Web_Application
+{
+    public class Context:DbContext
+    {
+        public DbSet<Models> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-6QHDMG7F;Database=Contacts;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ Address-Book-Web-Application/Services.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace Address_Book_Web_Application
+{
+    public class Services:IServices
+    {
+        private Context Db { get; set; }
+
+        public Services(Context context)
+        {
+            this.Db = context;
+        }
+        public bool AddNewContact(Models contact)
+        {
+            contact.Id = Guid.NewGuid().ToString();
+            this.Db.Contacts.Add(contact);
+            return this.Db.SaveChanges() >0;
+        }
+
+        public Models GetContactById(string id)
+        {
+            return this.Db.Contacts?.FirstOrDefault(a => a.Id == id);
+        }
+
+        public List<Models> GetAllContacts()
+        {
+            return this.Db.Contacts?.ToList();
+        }
+
+        public bool DeleteContact(string id)
+        {
+            this.Db.Contacts.Remove(this.Db.Contacts?.FirstOrDefault(a => a.Id == id));
+            return this.Db.SaveChanges()>0;
+        }
+
+        public bool EditContact(Models contact,string id)
+        {
+            var oldDetails = this.Db.Contacts?.FirstOrDefault(a => a.Id == id);
+            if (oldDetails != null)
+            {
+                oldDetails.Name=contact.Name;
+                oldDetails.Mobile=contact.Mobile;
+                oldDetails.Landline=contact.Landline;
+                oldDetails.Address=contact.Address;
+                oldDetails.Email=contact.Email; 
+                return this.Db.SaveChanges() > 0;
+            }
+            return false;
+        }
+    }
+}
